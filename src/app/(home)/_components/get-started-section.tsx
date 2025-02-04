ed862065cc7829480e92157dd99be6a6533a04bd
Josh Miller <joshuae.miller100@gmail.com>
2025-02-04 13:54:50 +0100

    Update GetStartedSection component styling
    - Remove container mx-auto class from parent div
    - Add center alignment to TitleBlock component
    - Update TitleBlock classNames to center text and set max-width
    - Comment out LeadForm component

--- src/app/(home)/_components/get-started-section.tsx
+++ src/app/(home)/_components/get-started-section.tsx
@@ -25,12 +25,16 @@ export function GetStartedSection(props: GetStartedSectionProps) {
         playsInline
       />
       <div className="absolute inset-0 bg-black/70">
-        <div className="container mx-auto flex h-full flex-col items-center justify-center gap-8">
+        <div className="container flex h-full flex-col items-center justify-center gap-8">
           <TitleBlock
             {...props}
             size="lg"
-            className="max-w-[740px] text-white [&>h2]:text-white/60"
+            align="center"
+            classNames={{
+              container: "max-w-[740px] mx-auto text-center",
+            }}
           />
+
           {/* <LeadForm
             source={discoveryCallBookingSource.bookingUrl}
             id={discoveryCallBookingSource.bookingId}
