eb031d955d16564297d7e08fdcd6be05130367c0
Taki93157 <kelsey.abreu6767@gmail.com>
2025-04-04 07:54:23 -0400

    Test Vercel Deploy

--- src/app/video-platform/layout.tsx
+++ src/app/video-platform/layout.tsx
@@ -1,11 +1,7 @@
-import type React from "react"
+import type React from "react";
 import "@/styles/globals.css";
 import { plusJakartaSansFont, jetBrainsMonoFont } from "@/app/font";
-
 import { Analytics } from "@vercel/analytics/react";
-
-import { ThemeProvider } from "@/components/video-platform/theme-provider"
-import { AuthProvider } from "@/components/video-platform/auth-provider"
 
 export { meta as metadata } from "@/app/metadata";
 
@@ -14,13 +10,5 @@ type VideoPlatformLayoutProps = Readonly<{
 }>;
 
 export default function VideoPlatformLayout({ children }: VideoPlatformLayoutProps) {
-  return (
-    <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-      </ThemeProvider>
-    </>
-  );
+  return <>{children}</>;
 }
